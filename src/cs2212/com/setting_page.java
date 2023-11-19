c02291bad213425437b11be7a409528c20879a2c
yiffgithub <141269511+yiffgithub@users.noreply.github.com>
2023-11-18 22:05:18 -0500

    Update setting_page.java

--- src/cs2212/com/setting_page.java
+++ src/cs2212/com/setting_page.java
@@ -3,65 +3,67 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
 
 public class setting_page {
     private JFrame frame;
-//如果这个页面作为一个页面的子页面是不需要main方法的
-    public setting_page() {
-        frame = new JFrame("Setting");
-        frame.setSize(962, 779);
-        frame.setLayout(null);
+    private JDialog dialog;
+
+    public setting_page(Frame owner) {  // 使用 Frame 参数来指定父窗口
+        dialog = new JDialog(owner, "Setting", Dialog.ModalityType.APPLICATION_MODAL);
+        dialog.setSize(962, 779);
+        dialog.setLayout(null);
 
         JLabel settingLabel = new JLabel("Setting");
         settingLabel.setBounds(50, 40, 200, 40);
         settingLabel.setFont(new Font("Segoe UI", Font.BOLD, 34));
-        frame.add(settingLabel);
+        dialog.add(settingLabel);
 
         JLabel userCustomLabel = new JLabel("User customizable themes");
         userCustomLabel.setBounds(50, 102, 300, 20);
         userCustomLabel.setFont(new Font("Segoe UI", Font.PLAIN, 18));
         userCustomLabel.setForeground(new Color(0xA5A5A5));
-        frame.add(userCustomLabel);
+        dialog.add(userCustomLabel);
 
         JPanel theme1Panel = new JPanel();
         theme1Panel.setBounds(50, 146, 862, 275);
         theme1Panel.setBackground(new Color(0, 0, 0, 0x0A));
-        frame.add(theme1Panel);
+        dialog.add(theme1Panel);
 
         JLabel theme1Label = new JLabel("Theme 1");
         theme1Label.setBounds(81, 151, 130, 40);
         theme1Label.setFont(new Font("Segoe UI", Font.BOLD, 32));
-        frame.add(theme1Label);
+        dialog.add(theme1Label);
 
         JLabel fontSize1Label = new JLabel("Font size: Middle");
         fontSize1Label.setBounds(158, 226, 200, 20);
         fontSize1Label.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-        frame.add(fontSize1Label);
+        dialog.add(fontSize1Label);
 
         JLabel highlightColor1Label = new JLabel("High light color: Red");
         highlightColor1Label.setBounds(158, 264, 300, 20);
         highlightColor1Label.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-        frame.add(highlightColor1Label);
+        dialog.add(highlightColor1Label);
 
         JPanel theme2Panel = new JPanel();
         theme2Panel.setBounds(50, 450, 862, 275);
         theme2Panel.setBackground(new Color(0, 0, 0, 0x0A));
-        frame.add(theme2Panel);
+        dialog.add(theme2Panel);
 
         JLabel theme2Label = new JLabel("Theme 2");
         theme2Label.setBounds(81, 455, 130, 40);
         theme2Label.setFont(new Font("Segoe UI", Font.BOLD, 32));
-        frame.add(theme2Label);
+        dialog.add(theme2Label);
 
         JLabel fontSize2Label = new JLabel("Font size: Large");
         fontSize2Label.setBounds(158, 530, 200, 20);
         fontSize2Label.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-        frame.add(fontSize2Label);
+        dialog.add(fontSize2Label);
 
         JLabel highlightColor2Label = new JLabel("High light color: Green");
         highlightColor2Label.setBounds(158, 568, 300, 20);
         highlightColor2Label.setFont(new Font("Segoe UI", Font.PLAIN, 20));
-        frame.add(highlightColor2Label);
+        dialog.add(highlightColor2Label);
 
         JButton setThemeButton1 = new JButton("Set theme");
         setThemeButton1.setBounds(688, 341, 200, 54);
@@ -70,10 +72,10 @@ public class setting_page {
         setThemeButton1.setForeground(Color.WHITE);
         setThemeButton1.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame.dispose();
+                dialog.dispose();
             }
         });
-        frame.add(setThemeButton1);
+        dialog.add(setThemeButton1);
 
         JButton setThemeButton2 = new JButton("Set theme");
         setThemeButton2.setBounds(688, 646, 200, 54);
@@ -82,20 +84,25 @@ public class setting_page {
         setThemeButton2.setForeground(Color.WHITE);
         setThemeButton2.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent e) {
-                frame.dispose();
+                dialog.dispose();
             }
         });
-        frame.add(setThemeButton2);
+        dialog.add(setThemeButton2);
 
         JPanel mainBg = new JPanel();
         mainBg.setBounds(0, 0, 962, 779);
         mainBg.setBackground(new Color(0xF6EEFF));
-        frame.add(mainBg);
+        dialog.add(mainBg);
 
-        frame.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
+
+        dialog.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
     }
 
     public void setVisible(boolean b) {
-        frame.setVisible(b);
+        dialog.setVisible(b);
+    }
+
+    public void addWindowListener(WindowListener listener) {
+        dialog.addWindowListener(listener);
     }
 }
